054b827e3409e2c75ff0b1e175ef0cfb4c9d38b8
Jair Nunes <jair@cartorio.fortaleza>
2026-01-13 15:31:43 -0300

    Mais estilos no Dashboard, adicionado manipulação de navegação ativa

--- digital-atm/frontend/app/js/dashboard.js
+++ digital-atm/frontend/app/js/dashboard.js
@@ -7,10 +7,12 @@ buttons.forEach(button => {
 
 function panelActive(event) {
     panels.forEach(panel => panel.classList.remove('active'));
+    buttons.forEach(btn => btn.classList.remove('nav-active'));
 
     const button = event.currentTarget;
     const targetId = button.dataset.target;
     const section = document.getElementById(targetId);
-
+    
+    button.classList.add('nav-active');
     section.classList.add('active');
 }
